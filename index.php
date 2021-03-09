d8dd0a81d15bab773a4dd1244086b84c5a3e89ce
nedimbandzovic <nedim.bandzovic@stu.ibu.edu.ba>
2021-03-09 20:55:45 +0100

    mad libs game

--- index.php
+++ index.php
@@ -2,92 +2,35 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Mad Libs Game</title>
   </head>
   <body>
+
+    <?php echo ("<h1> Mad Libs Game </h1>"); ?>
+
+
+
+    <form action="index.php" method="get">
+      Color: <input type="text" name="color">
+      <br>
+      Plural noun: <input type="text" name="pluralnoun">
+      <br>
+      Celebrity: <input type="text" name="celebrity">
+      <br>
+      <input type="submit">
+    </form>
+    <br>
+
+
 <?php
-echo ("<h1>Mini Calculator</h1>");
-echo ("<p> Addition </p>");
-?>
+$color=$_GET["color"];
+$pluralnoun=$_GET["pluralnoun"];
+$celebrity=$_GET["celebrity"];
 
-<form action="index.php" method="get">
-  <input type="number" name="num1">
-  <br>
-  <input type="number" name="num2">
-  <br>
-  <input type="submit">
-</form>
-
-SUM: <?php
-$number1= $_GET["num1"];
-$number2= $_GET["num2"];
-$number3=$number1+$number2;
-echo("$number3");
-
+echo ("Roses are $color <br>");
+echo ("$pluralnoun are blue <br>");
+echo ("I love you $celebrity <br>");
  ?>
-
- <hr>
-
- <?php echo ("<p>Subtraction</p>"); ?>
-
-
- <form action="index.php" method="get">
-   <input type="number" name="num3">
-   <br>
-   <input type="number" name="num4">
-   <br>
-   <input type="submit">
-</form>
-
-SUBTRACTION: <?php
-$number4=$_GET["num3"];
-$number5=$_GET["num4"];
-$number6=$number4-$number5;
-
-echo("$number6");
- ?>
-
-<hr>
-
-<?php echo ("<p> Multiplication </p>"); ?>
-
-<form action="index.php" method="get">
-  <input type="number" name="br1">
-  <br>
-  <input type="number" name="br2">
-  <br>
-  <input type="submit">
-</form>
-
-MULTIPLICATION: <?php
-
-$broj1=$_GET["br1"];
-$broj2=$_GET["br2"];
-$broj3=$broj1*$broj2;
-echo ("$broj3");
-
-
-
- ?>
- <hr>
-
-<?php echo ("<p> Division </p>"); ?>
-
-<form action="index.php" method="get">
-  <input type="number" name="brO1">
-  <br>
-  <input type="number" name="brO2">
-  <br>
-  <input type="submit">
-</form>
-
-DIVISION: <?php
-$bre1=$_GET["brO1"];
-$bre2=$_GET["brO2"];
-$bre3=$bre1/$bre2;
-echo ("$bre3");
-?>
-
 
   </body>
 </html>
