b13c9d55fdb6891c2356fd30bcc73d0defb4ed67
nedimbandzovic <nedim.bandzovic@stu.ibu.edu.ba>
2021-03-10 13:49:45 +0100

    post practice

--- index.php
+++ index.php
@@ -2,35 +2,23 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Mad Libs Game</title>
+    <title>Coronavirus Assistant - BiH</title>
   </head>
   <body>
-
-    <?php echo ("<h1> Mad Libs Game </h1>"); ?>
-
-
-
-    <form action="index.php" method="get">
-      Color: <input type="text" name="color">
-      <br>
-      Plural noun: <input type="text" name="pluralnoun">
-      <br>
-      Celebrity: <input type="text" name="celebrity">
-      <br>
-      <input type="submit">
-    </form>
-    <br>
+<?php
+echo ("<h1>Dummy program</h1> <br>");
+echo("Enter your password");
+ ?>
+ <br>
+ <form action="index.php" method="post">
+  Password: <input type="password" name="password">
+   <br>
+   <input type="submit">
 
 
 <?php
-$color=$_GET["color"];
-$pluralnoun=$_GET["pluralnoun"];
-$celebrity=$_GET["celebrity"];
 
-echo ("Roses are $color <br>");
-echo ("$pluralnoun are blue <br>");
-echo ("I love you $celebrity <br>");
- ?>
+echo $_POST("password"); ?>
 
   </body>
 </html>
