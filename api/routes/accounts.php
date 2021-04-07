8824f34b78fd1510a9b546dfe55211d07081aa7c
nedimbandzovic <nedim.bandzovic@stu.ibu.edu.ba>
2021-04-07 12:34:02 +0200

    GET Accounts on Swagger

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -11,19 +11,23 @@
 
 /**
  * @OA\Get(
- *     path="/accounts,
- *     @OA\Response(response="200", description="Get registered accounts from DB")
+ *     path="/accounts",tags={"account"},
+ *        @OA\Parameter(type="integer", in="query", name="offset", default=0, description="Offset for pagination"),
+ *     @OA\Parameter(type="integer", in="query", name="limit", default=25, description="Limit for pagination"),
+ *     @OA\Parameter(type="string", in="query", name="search", description="Search string for accounts. Case insensitive search."),
+ *     @OA\Parameter(type="string", in="query", name="order", default="-id", description="Sorting for return elements. -column_name ascending order by column_name or +column_name descending order by column_name"),
+ *     @OA\Response(response="200", description="List accounts from database")
  * )
  */
+Flight::route('GET /accounts', function(){
+  $offset = Flight::query('offset', 0);
+  $limit = Flight::query('limit', 25);
+  $search = Flight::query('search');
+  $order = Flight::query('order', "-id");
 
- Flight::route('GET /admin/accounts', function(){
-   $offset = Flight::query('offset', 0);
-   $limit = Flight::query('limit', 25);
-   $search = Flight::query('search');
-   $order = Flight::query('order', "-id");
+  Flight::json(Flight::accountService()->get_accounts($search, $offset, $limit, $order));
+});
 
-   Flight::json(Flight::accountService()->get_accounts($search, $offset, $limit, $order));
- });
 /**
  * @OA\Get(path="/admin/accounts/{id}",
  *     @OA\Parameter(type="integer", in="path", name="id", example=1, description="Id of account"),
