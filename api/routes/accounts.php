81a408efccf984e3997b89d30e0423c99b0c07f0
nedimbandzovic <nedim.bandzovic@stu.ibu.edu.ba>
2021-04-07 10:57:11 +0200

    Add Swagger annotations

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -2,13 +2,13 @@
 
 
 /**
- * @OA\Info(title="My First API", version="0.1")
+ * @OA\Info(title="Coronavirus Assistant API", version="0.1")
  */
 
 /**
  * @OA\Get(
- *     path="/api/resource.json",
- *     @OA\Response(response="200", description="An example resource")
+ *     path="/accounts",
+ *     @OA\Response(response="200", description="Get registered accounts from DB")
  * )
  */
 
@@ -27,19 +27,35 @@ Flight::route('GET /accounts', function(){
 
 });
 
+/**
+ * @OA\Get(path="/admin/accounts/{id}",
+ *     @OA\Parameter(type="integer", in="path", name="id", default=1, description="Id of account"),
+ *     @OA\Response(response="200", description="Fetch individual account")
+ * )
+ */
 
 Flight::route('GET /accounts/@id', function($id){
 
     Flight::json(Flight::accountService()->get_by_id($id));
 });
-
+/**
+ * @OA\Post(path="/admin/accounts",
+ *     
+ *     @OA\Response(response="200", description="Add new account")
+ * )
+ */
 Flight::route('POST /accounts', function(){
     $data=Flight::request()->data->getData();
     Flight::json(Flight::accountService()->add($data));
 
 
 });
-
+/**
+ * @OA\Put(path="/admin/accounts",
+ *     @OA\Parameter(type="integer", in="path", name="id", default=1, description="Id of account"),
+ *     @OA\Response(response="200", description="Update account via ID")
+ * )
+ */
 Flight::route('PUT /accounts/@id', function($id){
 
 
--- api/test.php
+++ api/test.php
@@ -4,6 +4,6 @@
 require_once dirname (__FILE__)."/../vendor/autoload.php";
 $openapi = \OpenApi\scan(dirname(__FILE__)."/routes");
 header('Content-Type: application/json');
-echo $openapi->toYaml();
+echo $openapi->toJson();
 
  ?>
