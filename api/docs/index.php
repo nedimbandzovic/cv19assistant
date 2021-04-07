57d7680d9b1f10245370f3b600aece22a710afb3
nedimbandzovic <nedim.bandzovic@stu.ibu.edu.ba>
2021-04-07 11:08:32 +0200

    Swagger UI slight modifications

--- api/docs/index.php
+++ api/docs/index.php
@@ -1,19 +1,11 @@
-<?php
-/* Project configuration */
-require_once __DIR__ . '/../config/config.php';
-?>
-
 <!-- HTML for static distribution bundle build -->
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
-    <title><?php echo PROJECT_DOCS_TITLE ?></title>
-    <link rel="stylesheet" type="text/css" href="<?php echo DOCS_FOLDER ?>/swagger-ui.css">
-    <link rel="stylesheet" type="text/css" href="<?php echo DOCS_FOLDER ?>/custom-swagger.css">
-    <link rel="icon" type="image/png" href="<?php echo DOCS_FOLDER ?>/favicon-32x32.png" sizes="32x32" />
-    <link rel="icon" type="image/png" href="<?php echo DOCS_FOLDER ?>/favicon-16x16.png" sizes="16x16" />
+    <title>Coronavirus Assistant API</title>
+    <link rel="stylesheet" type="text/css" href="swagger-ui.css">
+    <link rel="icon" type="image/png" href="favicon-32x32.png" sizes="32x32" />
     <style>
         html {
             box-sizing: border-box;
@@ -36,14 +28,12 @@ require_once __DIR__ . '/../config/config.php';
 
 <body>
     <div id="swagger-ui"></div>
-
-    <script src="<?php echo DOCS_FOLDER ?>/swagger-ui-bundle.js"> </script>
-    <script src="<?php echo DOCS_FOLDER ?>/swagger-ui-standalone-preset.js"> </script>
+    <script src="swagger-ui-bundle.js"> </script>
+    <script src="swagger-ui-standalone-preset.js"> </script>
     <script>
         window.onload = function () {
-            // Begin Swagger UI call region
             const ui = SwaggerUIBundle({
-                url: "<?php echo DOCS_FOLDER ?>/swagger.json",
+                url: "http://localhost/cv19assistant/api/test.php",
                 dom_id: '#swagger-ui',
                 deepLinking: true,
                 presets: [
@@ -55,8 +45,6 @@ require_once __DIR__ . '/../config/config.php';
                 ],
                 layout: "StandaloneLayout"
             })
-            // End Swagger UI call region
-
             window.ui = ui
         }
     </script>
