8a426c151a645ea2b80c760c6a6c08e542f4c437
nedimbandzovic <nedim.bandzovic@stu.ibu.edu.ba>
2021-04-07 10:44:50 +0200

    Install Swagger

--- api/routes/accounts.php
+++ api/routes/accounts.php
@@ -1,7 +1,16 @@
 <?php
 
 
+/**
+ * @OA\Info(title="My First API", version="0.1")
+ */
 
+/**
+ * @OA\Get(
+ *     path="/api/resource.json",
+ *     @OA\Response(response="200", description="An example resource")
+ * )
+ */
 
 
 Flight::route('GET /accounts', function(){
---
+++ api/test.php
@@ -0,0 +1,9 @@
+
+<?php
+
+require_once dirname (__FILE__)."/../vendor/autoload.php";
+$openapi = \OpenApi\scan(dirname(__FILE__)."/routes");
+header('Content-Type: application/json');
+echo $openapi->toYaml();
+
+ ?>
