5b6d7b7a478708e26c837f0f547b468ad2dc7044
nedimbandzovic <nedim.bandzovic@stu.ibu.edu.ba>
2021-06-15 12:27:00 +0200

    add jsonize form

--- assets/js/utils.js
+++ assets/js/utils.js
@@ -1,11 +1,8 @@
-function jsonize_form(selector){
-
+function jsonize_form(selector) {
   var data = $(selector).serializeArray();
-      var form_data = {};
-      for(var i = 0; i < data.length; i++){
-        form_data[data[i].name] = data[i].value;
-      }
-      return form_data;
-
-
+  var form_data = {};
+  for(var i = 0; i < data.length; i++){
+    form_data[data[i].name] = data[i].value;
+  }
+  return form_data;
 }
